8ffbb803c2e1de499aa6903bf0d9a44e9675119a
Tishana B. <t.bascombe@outlook.com>
2025-05-06 21:03:32 +0000

    add links for css and js to html doc, and fix formatting of docs

--- scripts/app.js
+++ scripts/app.js
@@ -2,17 +2,17 @@
 let changeAppearanceButton = document.querySelector(".dark-mode-toggle");
 changeAppearanceButton.addEventListener("click", changeAppearance);
 
-      function changeAppearance() {
-        let body = document.querySelector("body");
+function changeAppearance() {
+  let body = document.querySelector("body");
 
-        if (body.classList.contains("dark-mode")) {
-          body.classList.remove("dark-mode");
-        } else {
-          body.classList.add("dark-mode");
-        }
-      }
+  if (body.classList.contains("dark-mode")) {
+    body.classList.remove("dark-mode");
+  } else {
+    body.classList.add("dark-mode");
+  }
+}
 
-      // Navigate to website to watch kdramas
-      function goToViki() {
-        window.location.href = "https://www.viki.com/";
-      }
+// Navigate to website to watch kdramas
+function goToViki() {
+  window.location.href = "https://www.viki.com/";
+}
